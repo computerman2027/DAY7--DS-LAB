7f5703eca61b0b4e5e0b8913d54b11710e6aa60b
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2024-09-09 16:30:48 +0530

    COMMIT 2

---
+++ p7.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#define MAXITEM 20
+
+int maxarr(int *arr, int n)
+{
+    if (n == 1)
+        return arr[0];
+    int m = maxarr(arr + 1, n - 1);
+    return arr[0] < m ? arr[0] : m;
+}
+
+int main()
+{
+    int i,n;
+    int arr[MAXITEM];
+    printf("Enter size of array : ");
+    scanf("%d", &n);
+    if (n < 1 || n > MAXITEM)
+    {
+        printf("INVALID SIZE\n");
+        return 0;
+    }
+    printf("Enter %d numbers : ", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("ARRAY = ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    printf("Min = %d\n",maxarr(arr,n));
+    return 0;
+}
